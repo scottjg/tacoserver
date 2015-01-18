f10d642269eff7ccbcb0a10f1f891f446384744b
Scott J. Goldman <scottjg@github.com>
2015-01-17 23:30:39 -0800

    animate the telnet taco

--- telnet.go
+++ telnet.go
@@ -4,7 +4,9 @@ import (
 	"fmt"
 	"log"
 	"net"
+	"io"
 	"strings"
+	"time"
 )
 
 func telnetServer() {
@@ -28,7 +30,37 @@ func telnetServer() {
 			defer conn.Close()
 
 			log.Printf("New telnet connection from %s", conn.RemoteAddr())
-			fmt.Fprintf(conn, strings.Replace(Taco, "\n", "\r\n", -1))
+			for {
+				_, err := fmt.Fprintf(conn, strings.Replace(Taco1, "\n", "\r\n", -1))
+				if err != nil {
+					break
+				}
+				time.Sleep(time.Second)
+				_, err = fmt.Fprintf(conn, "\033[10A" + strings.Replace(Taco2, "\n", "\r\n", -1))
+				if err != nil {
+					break
+				}
+				time.Sleep(time.Second)
+				fmt.Fprintf(conn, "\033[10A")
+			}
+		}(conn)
+
+		go func (conn net.Conn) {
+			defer conn.Close()
+			tmp := make([]byte, 256)
+			for {
+				n, err := conn.Read(tmp)
+				if err == io.EOF {
+					break
+				}
+
+				// ctrl-c
+				if n == 5 &&
+				   tmp[0] == 255 && tmp[1] == 244 && tmp[2] == 255 &&
+				   tmp[3] == 253 && tmp[4] == 6 {
+					break
+				}
+			}
 		}(conn)
 	}
 }
